86f62ca8dabe5885154d5a67406b0ddf6f431edc
Sachith Ariyathilaka <sachith.vidu@yahoo.com>
2024-05-15 18:54:19 +0530

    Material UI integrated

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,61 @@
-import React, { Component } from 'react';
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import {Component} from "react";
 
-class App extends Component{
-  render(){
+class App extends Component {
+
+    constructor(props, context)
+    {
+        super(props, context);
+        this.state = {
+            users: []
+        }
+    }
+
+    componentDidMount()
+    {
+        this.setState({users: ["A", "B"]})
+    }
+
+    render(){
     return(
-        <div>
-          <h1>JavaTpoint</h1>
-          <h2>Training Institutes</h2>
-          <p>This website contains the best CS tutorials.</p>
-        </div>
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Dessert (100g serving)</TableCell>
+                        <TableCell align="right">Calories</TableCell>
+                        <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                        <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                        <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {this.state.users.map((row) => (
+                        <TableRow
+                            key={row}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                            <TableCell component="th" scope="row">
+                                {row}
+                            </TableCell>
+                            <TableCell align="right">{row}</TableCell>
+                            <TableCell align="right">{row}</TableCell>
+                            <TableCell align="right">{row}</TableCell>
+                            <TableCell align="right">{row}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
     );
   }
 }
+
 export default App;
