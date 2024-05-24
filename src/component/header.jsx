24e60f6fac2a82af3f97b5a53cc9150e587d25e2
sachith.vidu@yahoo.com <sachith.vidu@yahoo.com>
2024-05-24 15:27:07 +0530

    Header component implemented

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -1,22 +1,28 @@
 import {Component} from "react";
 import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
 import * as React from "react";
+import "../asset/css/header.css"
+import Toolbar from "@mui/material/Toolbar";
+import {Breadcrumbs, Button, Link} from "@mui/material";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
 export class Header extends Component {
 
-    sidebarWidth = window.innerWidth * 1.5/12;
+    addNewBook() {
+        alert('test')
+    }
 
     render() {
         return(
-            <AppBar
-                position="fixed"
-                sx={{ width: `calc(100% - ${this.sidebarWidth}px)`}}>
-                <Toolbar>
-                    <Typography variant="h6" noWrap component="div">
-                        Permanent drawerrr
-                    </Typography>
+            <AppBar position="fixed" className={'app-bar'}>
+                <Toolbar className = {'header-container'}>
+                    <Breadcrumbs aria-label="breadcrumb">
+                        <Link underline="hover" color="inherit" href="/" className={'breadcrumbs-text'}>Digital Library</Link>
+                        <Typography color="text.primary" className={'breadcrumbs-text'}>Books</Typography>
+                    </Breadcrumbs>
+                    <Box sx={{ flexGrow: 1 }} />
+                    <Button variant="contained" className={'toolbar-btn'} onClick={this.addNewBook}>Add New Book</Button>
                 </Toolbar>
             </AppBar>
         );
